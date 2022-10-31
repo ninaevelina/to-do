3779bf853bfeb574b24ee9d47476ba62dab5bab8
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2022-10-31 14:14:20 +0100

    adj html, added empty ul to create li with js

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Variables
+
+let taskForm = document.getElementById("taskForm"); // Variable for form-element
+let taskInput = document.getElementById("taskInput"); // Variable for input-element
+
+// Todo - create list for the form, input, current tasks and completed
+
+// Function
+
+// Toggle-function
