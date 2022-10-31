18863791a9dc90e95569bb574abd13ef5e709e37
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2022-10-31 17:28:35 +0100

    added script to head, added var+class constr

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,22 @@
 // Variables
 
+let taskToDoList = [];
+
 let taskForm = document.getElementById("taskForm"); // Variable for form-element
-let taskInput = document.getElementById("taskInput"); // Variable for input-element
+let taskDate = document.getElementById("taskDate"); // Variable for input element type date
+let taskInput = document.getElementById("taskInput"); // Variable for input-element type text
+let currentTask = document.getElementById("taskListCurrent"); // Variable for my current tasks ul-element
+let taskComplete = document.getElementById("completedTask"); // Variable for my completed tasks ul-element
+
+// Class for tasks, constructor connected to the two inputs - date and text (text is taskdetails property and parameter)
+
+class Task {
+  constructor(date, taskdetails) {
+    this.date = date;
+    this.taskdetails = taskdetails;
+    //lägg till en funktion för om task är completed true/false
+  }
+}
 
 // Todo - create list for the form, input, current tasks and completed
 
