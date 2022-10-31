025b14229ee5bc420b65aa905687dd6c493b6ded
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2022-10-31 17:56:23 +0100

    added base of function for adding task

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,22 @@ class Task {
   }
 }
 
+// Create objects based on the above class
+
+let portfolio = new Task("2022-11-05", "Update portfolio");
+let coffee = new Task("2022-11-06", "Buy a coffee-thermos");
+let drive = new Task("2024-01-01", "Get your driver's license");
+
+taskForm.addEventListener("submit", addTask); // add eventlistener for the form, when user input is submitted in the form, call the function (which entails to add task to "my current tasks")
+
+function addTask(e) {
+  // e is object which contains information about the event
+  let newTask = new Task();
+  newTask.date = taskDate.value;
+  newTask.taskdetails = taskInput.value;
+  taskToDoList.push(newTask);
+}
+
 // Todo - create list for the form, input, current tasks and completed
 
 // Function
